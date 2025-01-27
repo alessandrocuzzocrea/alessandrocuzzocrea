04a3def07c9babc26b275ee95cb3007e0156381b
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-01-27 02:54:08 +0000

    k

--- update.py
+++ update.py
@@ -1,15 +1,11 @@
 import feedparser
 
-# URL of the RSS feed
 rss_url = "https://alessandrocuzzocrea.com/feed.xml"
 
-# Parse the RSS feed
 feed = feedparser.parse(rss_url)
 
-# Prepare the file content
 file_content = "### Latest articles\n\n"
 
-# Get the latest 5 articles
 latest_articles = feed.entries[:5]
 
 for entry in latest_articles:
@@ -17,7 +13,6 @@ for entry in latest_articles:
     link = entry.link
     file_content += f"- #### [{title}]({link})\n"
 
-# Write the content to a file
 with open("README.md", "w", encoding="utf-8") as file:
     file.write(file_content)
 
